780eb9278276e1e2cbb9ac07243db291fd897926
Dajuan Mcdonald <flopwiki@gmail.com>
2020-04-18 12:59:36 -0400

    added post, put and delete logic

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const server = express();
 
 server.use(express.json());
 
+
+//Returns an array of all the user objects contained in the database.
 server.get('/api/users', (req, res) => {
     dataBase.find()
     .then( user => {
@@ -17,6 +19,7 @@ server.get('/api/users', (req, res) => {
 
 })
 
+// Returns the user object with the specified id
 server.get('/api/users/:id', (req, res) => {
     dataBase.findById(req.params.id)
     .then( user => {
@@ -34,6 +37,28 @@ server.get('/api/users/:id', (req, res) => {
     
 })
 
+//Creates a user using the information sent inside the `request body`.
+server.post('/api/users', (req, res) => {
+    dataBase.insert(req.params.body)
+    .then( user => {
+        res.status(201).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({errorMessage: "The user information could not be modified."})
+    })
+})
+
+// Updates the user with the specified `id` using data from the `request body`.
+server.put('/api/users/:id', (req, res) => {
+    dataBase.update(req.params.body)
+})
+
+//Removes the user with the specified `id`
+server.delete('/api/users/:id', (req, res) => {
+    dataBase.remove(req.params.id)
+})
+
+// The server is configured to restart automatically as you make changes.
 server.listen(3000, () => {
     console.log('Server is up and running on port 3000.')
 })
