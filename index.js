96ce3c87207a5a87f0e3f6ac127b95fb4164910f
Dajuan Mcdonald <flopwiki@gmail.com>
2020-04-17 23:06:15 -0400

    added get methods to index.js

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // implement your API here
+const express = require('express');
+const dataBase = require('./data/db');
+const server = express();
+
+server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+    dataBase.find()
+    .then( user => {
+        res.status(200).json(user);
+
+    })
+    .catch(err => {
+        res.status(500).json({message: "The users information could not be retrieved."})
+    })
+
+})
+
+server.get('/api/users/:id', (req, res) => {
+    dataBase.findById(req.params.id)
+    .then( user => {
+        if(user) {
+            res.status(200).json(user)
+
+        } else {
+            res.status(404).json({message: "user not found."})
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({message: "There was an error."})
+    })
+    
+})
+
+server.listen(3000, () => {
+    console.log('Server is up and running on port 3000.')
+})
